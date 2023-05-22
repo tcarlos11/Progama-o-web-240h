4068ee9f5b5e2c71d512e3329f11c338e67e822c
Antonio Carlos (Tcarlos) <124454152+tcarlos11@users.noreply.github.com>
2023-05-21 21:03:55 -0300

    Update conex.php

--- PROJETO FINAL DO CURSO/conex.php
+++ PROJETO FINAL DO CURSO/conex.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new MySQLi('localhost', 'id20527378_root', 's/q)pY@x)W2%4E2P', 'id20527378_montadora');
+$conn = new MySQLi('localhost', 'id_root', 'senha', 'id_montadora');
 if ($conn->connect_error) {
   die("falha da conexão" . $conn->connect_error); 
 }
